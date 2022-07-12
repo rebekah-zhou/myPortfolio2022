469009e69efa1eb6357e938d717d6bd21cb53604
Rebekah Zhou <rebekahzhou@gmail.com>
2022-07-12 14:20:26 -0700

    sand svg image

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import TopBar from './Components/TopBar/TopBar';
 import MainDisplay from './Components/MainDisplay/MainDisplay';
 import { useState } from 'react'
+import BottomBar from './Components/BottomBar/BottomBar';
 
 function App() {
   const [pageToDisplay, setPageToDisplay] = useState("Home")
@@ -10,7 +11,8 @@ function App() {
     <div id='app'>
       <TopBar onCloudClick={setPageToDisplay} onSunClick={setPageToDisplay}/>
       <MainDisplay pageToDisplay={pageToDisplay}/>
-      <div id='bottom-bar'><div className='pixelart'></div></div>
+      {/* <div id='bottom-bar'><div className='pixelart'></div></div> */}
+      <BottomBar />
     </div>
   );
 }
